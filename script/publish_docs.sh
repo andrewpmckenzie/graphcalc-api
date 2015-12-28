49fbc1a66e645041cb95d5494b524dfbcba9a21f
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-12-27 23:25:14 -0500

    Have travis publish docs on success.

---
+++ script/publish_docs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# inspired by http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+if [ "$TRAVIS_REPO_SLUG" == "andrewpmckenzie/graphcalc-api" ] && [ "$TRAVIS_NODE_VERSION" == "4.1" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Building docs...\n"
+  npm run doc
+
+  echo -e "Publishing docs...\n"
+  mv ./doc $HOME/api-docs
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/andrewpmckenzie/graphcalc-api gh-pages > /dev/null
+
+  cd gh-pages
+  find . -not -name '.git*' | xargs rm -r
+  cp -rf $HOME/api-docs/* .
+  git add -f .
+  git commit -m "Updating autogen'd docs for travis build $TRAVIS_BUILD_NUMBER."
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published docs to gh-pages."
+
+fi
