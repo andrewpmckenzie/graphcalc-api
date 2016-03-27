3fa319ef27a47bc26d4d727a0cf45d0326b39f37
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2016-03-27 13:27:17 -0400

    Accept options for graph requests.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var Server = require('./lib/Server');
 
-module.exports = function (options) {
-  return new Server(options).start();
-};
-
+module.exports = function (options) { return new Server(options).start(); };
+module.exports.middleware = function (options) { return new Server(options).app; };
 module.exports.Server = Server;
